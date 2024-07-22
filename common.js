0969989e4e9717bfd8c6faa553da596056e86a15
bvv-kj <150868487+bvv-kj@users.noreply.github.com>
2024-07-22 18:42:42 +0800

    Update common.js

--- common.js
+++ common.js
@@ -2,7 +2,7 @@
 function reUrl(){
     let reUrl = 'http://101.37.150.251:888';
     let currentUrl = window.location.href;
-    let newUrl = currentUrl.replace('https://bvv-kj.github.io', reUrl);
-    newUrl = newUrl.replace('http://bvv-kj.github.io', reUrl);
+    let newUrl = currentUrl.replace('https://bvv-kj.online', reUrl);
+    newUrl = newUrl.replace('http://bvv-kj.online', reUrl);
     window.location.replace(newUrl);
 }
